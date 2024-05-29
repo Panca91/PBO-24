2c48122c89381e835b71b6d57604587a1e605d19
Panca AR <pancarahman313@gmail.com>
2024-05-29 14:34:41 +0700

    tugas 1

---
+++ Tugas1/src/tugas1/Pakaian.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author M S I
+ */
+public interface Pakaian {
+    void Sepatu();
+    void Seragam();
+    void WarnaKaosKaki();
+}
